c7099053022b6a54303fd75e29c8e8b07af7844b
Xeviousbr <xeviousbr@gmail.com>
2024-04-09 02:36:44 -0300

    Pagina para cadastro

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Cadastro from './Cadastro.tsx'; 
 
 const App: React.FC = () => {
-  const handleClick = () => {
-    alert('Botão clicado!');
-  };
   return (
     <div>
-      <h1>Página Inicial</h1>
-      <button onClick={handleClick}>Clique Aqui</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
     </div>
   );
 };
+
+const HomePage: React.FC = () => {
+  let navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Catalogo de IAs Generativas</h1>
+      <button onClick={() => navigate('/cadastro')}>Ir para Cadastro</button>
+    </div>
+  );
+};
+
 export default App;
---
+++ client/src/Cadastro.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Cadastro: React.FC = () => {
+  return (
+    <div>
+      <h2>Tela de Cadastro</h2>
+      {/* Adicione os campos de cadastro aqui */}
+    </div>
+  );
+};
+
+export default Cadastro;
